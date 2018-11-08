e53971a18d672d7241dfea3849eeffbca854bb86
Ray S <rayseals@gmail.com>
2018-11-08 09:21:09 -0600

    Added Graphics.py
    Added Graphics.py so I could add a GUI

--- movieoftheday.py
+++ movieoftheday.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 import random
+import sqlite3
+from graphics import *
 
-movies = ['Star Wars 4,5 or 6','King Pin','Good Fellas','Casino','Tombstone','Big Trouble in Little China','O Brother where art though','True Lies','The Big Lebowski','Smokey and the Bandit','Independence Day','Vacations','Dumb and Dumber','Trading places','Ghost busters I or II','Thunder dome','Die hard','Pulp fiction','Hateful eight','Caddie shack','Stripes','Airplane','Apollo 13','Breakfast club','Animal house','Better off dead','Almost heros','Cabin boy','Young Frankenstein','Blazing saddles','Friday','Bull Durham','Romancing the Stone','Beverly Hills Cop','Fletch','Revenge of the Nerds','Step Brothers','talladega nights','Anchorman','Top Gun','Full metal jacket','Hunt for red October','Silence of the Lambs','Raising Arizona','Back to the future trilogy','Mall rats','Rocky universe','Shawshank redemption','Cool hand luke','So I married an axe murderer','Dodgeball','Sixteen Candles']
+def databasequery():
+    sqlite_file = 'movieoftheday.db'
+    table_name = 'movielist'
 
-numberofmovies = len(movies)
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
 
-secure_random = random.SystemRandom()
+    c.execute('select * from movielist order by random() limit 1;')
 
-movieoftheday = secure_random.choice(movies)
+    movie_name = c.fetchone()
+    
+    return movie_name
 
-def web_print_movie():
+    # c.execute('SELECT moviequotes.quote FROM movielist LEFT JOIN moviequotes ON moviequotes.movienumber = "movielist"."index" WHERE moviequotes.quote NOT NULL;')
+
+    # movie_quotes = c.fetchall()
+
+#def web_print_movie():
     #print ("Number of movies in the database: " + str(numberofmovies))
     #print ("<br>")
-    print("Your movie quote challenge is: " + movieoftheday)
+ #   print("Your movie quote challenge is: " + movieoftheday)
 
-web_print_movie()
+#web_print_movie()
 
-print (movieoftheday)
+#print (movieoftheday)
+ 
+movie_of_the_day = databasequery()
+
+print ('Movie of the day is:', movie_of_the_day)
+print ('')
+
+# print (movie_quotes)
